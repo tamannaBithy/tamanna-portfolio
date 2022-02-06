d1e25552bba35e14ddc22c1d2b942469992880fa
Tamanna14-cpu <bimurto.bithy@gmail.com>
2022-02-06 11:42:52 +0600

    added my profile

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
-import Image from 'next/image'
-import { Box, Container, Heading } from '@chakra-ui/react'
+import { Box, Container, Heading, Image, useColorModeValue } from '@chakra-ui/react'
 
 export default function Home() {
   return (
     <Container>
-      <Box borderRadius='lg' bg="red" mb={6} p={3} textAlign="center">
+      <Box borderRadius='lg' bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} mb={6} p={3} textAlign="center">
         Hello, I&apos;m a frontend developer based in Bangladesh!
       </Box>
 
@@ -15,6 +14,28 @@ export default function Home() {
           </Heading>
           <p>Digital Craftsman ( Developer / Designer / Barista )</p>
         </Box>
+
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          textAlign="center"
+        >
+
+          <Image
+
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            display="inline-block"
+            style={{ width: '104px', height: '110px', borderRadius: '50%' }}
+            src="/images/profile.jpg"
+            alt="Profile image"
+
+          ></Image>
+
+        </Box>
+
       </Box>
     </Container>
   )
